50ea6d426cc5e7a4d1eb6e45f3b2b20afe7dd6ac
Richard Garner <richard.garner@mq.edu.au>
2024-06-17 13:55:18 +1000

    Spacing changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,15 +8,15 @@
 </script>
 
 
-<div class="w-screen flex items-center justify-center">
+<div class="w-screen flex items-center justify-center px-4">
   <div class="max-w-prose mx-auto flex flex-col gap-1.5 py-5">
-  <h1 class="text-4xl text-center font-semibold text-red mt-3 mb-5">Australian Category Seminar</h1>
-  <div class="grid grid-cols-4 w-full text-center justify-stretch items-stretch gap-0.5">
-    <a class={path.endsWith('/') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/">About</a>
-    <a class={path.endsWith('recent-talks') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/recent-talks">Recent talks</a>
-    <a class={path.endsWith('past-talks') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/past-talks">Past talks</a>
-    <a class={path.endsWith('speakers') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/speakers">Speakers</a>
-  </div>
-  <slot />
+    <h1 class="text-4xl text-center font-semibold text-red mt-3 mb-5">Australian Category Seminar</h1>
+    <div class="grid grid-cols-4 w-full text-center justify-stretch items-stretch gap-0.5">
+      <a class={path.endsWith('/') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/">About</a>
+      <a class={path.endsWith('recent-talks') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/recent-talks">Recent talks</a>
+      <a class={path.endsWith('past-talks') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/past-talks">Past talks</a>
+      <a class={path.endsWith('speakers') ? "p-3 bg-sand font-medium" : "p-3 bg-sand-light"} href="{base}/speakers">Speakers</a>
+    </div>
+    <slot />
   </div>
 </div>
