88e94094e6ba758b457e9b9446af9001704a260b
Richard Garner <richard.garner@mq.edu.au>
2024-07-22 08:28:18 +1000

    Improve column spacing on desktop

--- src/routes/past-talks/+page.svelte
+++ src/routes/past-talks/+page.svelte
@@ -47,11 +47,11 @@
         <div class="mb-1 text-red font-bold">{date}</div>
         <ul class="flex flex-col gap-0.5">
           {#each data.grouped[year][date] as talk}
-            <li class="grid grid-cols-5 sm:grid-cols-4 w-full">
-              <a class="pr-2 col-span-2 sm:col-span-1 hover:text-red" href="{base}/speakers/{encodeURIComponent(String(talk.speaker))}">
+            <li class="grid grid-cols-5 sm:grid-cols-7 w-full">
+              <a class="pr-2 col-span-2 hover:text-red" href="{base}/speakers/{encodeURIComponent(String(talk.speaker))}">
                 {talk.speaker}
               </a>
-              <div class="col-span-3 text-pretty">
+              <div class="col-span-3 sm:col-span-5 text-pretty">
                 {talk.title}{#if talk.part}<span class="text-gray-500 pl-1">({talk.part}/{talk.totalParts})</span>{/if}
                 {#if talk.abstract}
                   <a class="text-deep-red hover:text-red" href="{base}/talks/{talk.abstract}">(abs)</a>
