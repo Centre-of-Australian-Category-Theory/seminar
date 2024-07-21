8763b439eb087a4495ac477cd62660dd2fd1518e
Richard Garner <richard.garner@mq.edu.au>
2024-07-21 15:38:17 +1000

    Add KaTeX support

--- src/routes/talks/[slug]/+page.server.js
+++ src/routes/talks/[slug]/+page.server.js
@@ -1,5 +1,68 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
+import katex from 'katex';
+
+function findEndOfMath(delimiter, text, startIndex) {
+  let index = startIndex;
+  let braceLevel = 0;
+
+  const delimLength = delimiter.length;
+
+  while (index < text.length) {
+    const character = text[index];
+
+    if (braceLevel <= 0 &&
+        text.slice(index, index + delimLength) === delimiter) {
+      return index;
+    } else if (character === "\\") {
+      index++;
+    } else if (character === "{") {
+      braceLevel++;
+    } else if (character === "}") {
+      braceLevel--;
+    }
+
+    index++;
+  }
+
+  return -1;
+};
+
+function renderString(text) {
+  let index;
+  let output = "";
+
+  while (true) {
+    index = text.search(/(\$\$|\$)/);
+    if (index === -1) {
+      break;
+    }
+
+    if (index > 0) {
+      output += text.slice(0, index);
+      text = text.slice(index); // now text starts with delimiter
+    }
+
+    const display = text.startsWith('$$');
+    const delimiter = display ? '$$' : '$';
+
+    index = findEndOfMath(delimiter, text, delimiter.length);
+    if (index === -1) {
+      break;
+    }
+
+    const math = text.slice(delimiter.length, index);
+
+    output += katex.renderToString(math, { displayMode: display, throwOnError: false });
+    text = text.slice(index + delimiter.length);
+  }
+
+  if (text !== "") {
+    output += text;
+  }
+
+  return output;
+};
 
 function humanizeDate(date) {
   const month = ['January', 'Feburary', 'March', 'April',
@@ -41,10 +104,9 @@ export function load({ params }) {
   {
     var paragraphs = abstract.split('\n\n');
     abstract = '<p class="pb-3">' + paragraphs.join('</p><p class="pb-3">') + '</p>';
-    console.log(abstract);
     abstract = abstract.replaceAll(/\((https?[^\s)]*)\)/g, '<a href="$1" class="px-0.5 text-deep-red hover:text-red">(link)</a>');
-    console.log(abstract);
-    talk.abstract = abstract.replaceAll(/([^"])(https?[^\s<)]*)/g, '$1<a href="$2" class="px-0.5 text-deep-red hover:text-red">(link)</a>');
+    abstract = abstract.replaceAll(/([^"])(https?[^\s<)]*)/g, '$1<a href="$2" class="px-0.5 text-deep-red hover:text-red">(link)</a>');
+    talk.abstract = renderString(abstract);
   }
 
   talk.humanDate = humanizeDate(talk.date);
