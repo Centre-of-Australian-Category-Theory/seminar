2254b3bbdfb36f031d17a4a7dca72640202628e0
Richard Garner <richard.garner@mq.edu.au>
2024-08-18 08:33:34 +1000

    Update organiser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
   The Australian Category Seminar is the weekly seminar of the
   Centre of Australian Category Theory. We meet each Wednesday
   afternoon at Macquarie University. The current organiser is
-  Soichiro Fujii. If you would like to sign up for seminar
+  JS Lemay. If you would like to sign up for seminar
   announcements, or if you would like to speak at the seminar, you
-  may contact Soichiro at
-  <a class="hover:text-red text-deep-red" href="mailto:s.fujii.math@gmail.com">s.fujii.math@gmail.com</a>.
+  may contact JS at
+  <a class="hover:text-red text-deep-red" href="mailto:js.lemay@mq.edu.au">js.lemay@mq.edu.au</a>.
 </div>
