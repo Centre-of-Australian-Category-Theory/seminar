aaa78aca4c679b32af6f1debfa5f13a6b8fd137d
Richard Garner <richard.garner@mq.edu.au>
2026-07-31 10:33:01 +1000

    Change organiser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
   The Australian Category Seminar is the weekly seminar of the
   Centre of Australian Category Theory. We meet each Wednesday
   afternoon at Macquarie University. The current organiser is
-  Marcello Lanfranchi. If you would like to sign up for seminar
+  Isaac Bankier. If you would like to sign up for seminar
   announcements, or if you would like to speak at the seminar, you
-  may contact Marcello at
-  <a class="hover:text-red text-deep-red" href="mailto:marcello.lanfranchi@mq.edu.au">marcello.lanfranchi@mq.edu.au</a>.
+  may contact Isaac at
+  <a class="hover:text-red text-deep-red" href="mailto:isaac.bankier@hdr.mq.edu.au">isaac.bankier@hdr.mq.edu.au</a>.
 </div>
